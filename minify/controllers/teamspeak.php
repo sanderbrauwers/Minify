c6203f11c23ea2d52c311fdd9841c6c7b746ce69
Sharpless512 <mahoney_be@hotmail.com>
2015-12-28 17:33:39 +0100

    Teamspeak module

---
+++ minify/controllers/teamspeak.php
@@ -0,0 +1,14 @@
+<?php
+class Teamspeak extends Controller {
+	function __construct() {
+		parent::__construct();
+		Auth::handleLogin();
+	}
+    
+	function index() {
+		$this->view->pageTitle = 'Teamspeak Center';
+		//$this->view->blogList = $this->model->blogList();					
+		$this->view->render('teamspeak/index');
+	}
+	
+}
---
+++ minify/views/teamspeak/index.php
@@ -0,0 +1,9 @@
+<h4>Maris aliquet diam eget tristique cursus.</h4>
+
+<p>Mauris molestie velit est, eu faucibus risus eleifend et. Praesent non augue eget justo pulvinar tempus. 
+	Phasellus rutrum arcu ut felis cursus ornare. Etiam eget feugiat arcu, et sagittis turpis. 
+	Praesent venenatis tortor at risus tristique, vel pulvinar massa vulputate. 
+	Quisque vestibulum, felis a ullamcorper congue, libero neque tincidunt purus, eget consectetur leo nulla in odio. Suspendisse potenti.
+</p>
+
+<a class="btn btn-default">Restart Teamspeak</a>
