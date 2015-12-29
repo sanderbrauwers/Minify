5ef64972255588f1ee88d0a257e9ad719d6ee5ac
Sharpless512 <mahoney_be@hotmail.com>
2015-12-29 17:23:00 +0100

    Shell

---
+++ install.sh
@@ -0,0 +1,5 @@
+apt-get -y update
+apt-get -y install apache2
+apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
+mysql_install_db
+apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
