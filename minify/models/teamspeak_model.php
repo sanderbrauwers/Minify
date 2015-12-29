f12e8941385b4dceebeb9f797ba9920c52b90089
Sharpless512 <mahoney_be@hotmail.com>
2015-12-29 18:29:27 +0100

    teamspeak create file

--- minify/models/teamspeak_model.php
+++ minify/models/teamspeak_model.php
@@ -4,23 +4,20 @@ class Teamspeak_Model extends Model{
 		parent::__construct();
 	}
 	
-	public function pageList(){
-		return $this->db->select('SELECT * FROM page');
-	}
-		
-	public function pageSingleList($pageid){	
-		return $this->db->select('SELECT * FROM page WHERE pageid = :pageid', 
-		array(':pageid' => $pageid));
-	}
+
 	    
 	public function restart(){
-		$postData = array(
-				'pageid' => $data['pageid'],
-      	'title' => $data['title'],
-     		'title_alias' => $data['title_alias'],
-				'content' => $data['content']
-			);
-        				
-		$this->db->update('page', $postData, "pageid = {$data['pageid']}");
+
+    echo getcwd() . "\n";
+
+		$response[] = array(
+                    array(
+                      'serverid'  => 1, 
+                      'status'    => 2)
+                  );
+
+    $fp = fopen('status.json', 'w');
+    fwrite($fp, json_encode($response));
+    fclose($fp);
 	}
 }
