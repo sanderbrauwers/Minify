b44297dd4b0a9beb53e0bd3300589fd803b60f5a
Sharpless512 <mahoney_be@hotmail.com>
2015-12-30 15:15:43 +0100

    Server.py

--- scripts/server.py
+++ scripts/server.py
@@ -1,9 +1,13 @@
-import json
+import json, os
 from pprint import pprint
 
 with open('../minify/data/status.json') as data_file:    
   data = json.load(data_file)
 
 for server in data:
-  pprint(server['serverid'])
-  open('sweg.txt', 'w')
+  print('Restarting server')
+  #pprint(server['serverid'])
+  print('Trying to delete json file..')
+  #os.remove('C:\wamp\www\webapp\minify\data')
+  os.remove('/var/www/html/webapp/minify/data/status.json')
+  print('Json succesfully deleted')
